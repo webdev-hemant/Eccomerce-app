5f9141d28dfa3f317015ca203ebb5e3205c1da33
webdev-hemant <webdev.hemant@gmail.com>
2022-09-29 22:10:28 +0530

    navbar changes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,53 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+
+interface INavroutes {
+  name: string;
+  route: string;
+}
 
 const Navbar = () => {
+  const activeStyle: React.CSSProperties = {
+    background: "black",
+    color: "white",
+  };
+
+  const activeClassName: string = "underline";
+
+  const NavRoutes: INavroutes[] = [
+    {
+      name: "Home",
+      route: "/",
+    },
+    {
+      name: "Checkout",
+      route: "/checkout",
+    },
+    {
+      name: "Unknown",
+      route: "/unknown",
+    },
+  ];
+
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/checkout">Checkout</Link>
-      </li>
-      <li>
-        <Link to="/nothing-here">Nothing Here</Link>
-      </li>
+    <nav>
+      <ul>
+        {NavRoutes.map((item) => (
+          <li key={item.name}>
+            <NavLink
+              end={item.route === "/"}
+              to={item.route}
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }
+            >
+              {item.name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
       <Outlet />
-    </ul>
+    </nav>
   );
 };
 
