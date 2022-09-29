4525ba23e4dbe3892b0e434a2f63a86d14438b3f
webdev-hemant <webdev.hemant@gmail.com>
2022-09-30 01:59:58 +0530

    portal and modal added

---
+++ src/components/Modal/MyModal.tsx
@@ -0,0 +1,12 @@
+import ReactPortal from "components/portal/ReactPortal";
+
+interface IProps {
+  children: JSX.Element;
+  open: boolean;
+}
+
+const MyModal = ({ children = <span></span>, open = false }: IProps) => {
+  return <ReactPortal targetDiv="myModal">{children}</ReactPortal>;
+};
+
+export default MyModal;
--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -1,10 +1,15 @@
 // import HomePageSkeleton from "components/skeletonLoaders/HomePageSkeleton";
 
+import MyModal from "components/Modal/MyModal";
+
 const Home = () => {
   return (
     <div>
       Home
       {/* <HomePageSkeleton /> */}
+      <MyModal open>
+        <h1>This is modal</h1>
+      </MyModal>
     </div>
   );
 };
