28ebbcbc699ef0ed8e010fa70524efbec0406bc9
webdev-hemant <webdev.hemant@gmail.com>
2022-09-30 18:38:10 +0530

    changes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -76,7 +76,7 @@ const Navbar = () => {
           <img src={shoppingcart} alt="" />
         </div>
       </nav>
-      <main style={{ padding: "0" }}>
+      <main className={styles.mainContainer}>
         <Outlet />
         <footer
           style={{
