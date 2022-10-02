af5e1e8c91ef9160c1633d2619817956e275b89f
webdev-hemant <webdev.hemant@gmail.com>
2022-10-02 17:41:34 +0530

    greeting modal completed

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -2,8 +2,12 @@ import React, { useCallback, useEffect, useState } from "react";
 import MyModal from "components/Modal/MyModal";
 import MySlider from "components/slider/MySlider";
 import Product from "components/Product/Product";
-import styles from "./home.module.scss";
 import { Productvariant } from "components/Product/ProductVariants";
+import OutsideClickHandler from "react-outside-click-handler";
+import welcomeImage from "images/welcome.png";
+import greetingThumb from "images/greetingthumb.png";
+import sadfaceImage from "images/sadface.png";
+import styles from "./home.module.scss";
 
 interface IApiData {
   id: number;
@@ -20,24 +24,39 @@ interface IApiData {
 
 const Home = () => {
   const [apiData, setApiData] = useState<IApiData[]>([]);
+  const [isOpenmodal, setIsOpenmodal] = useState<boolean>(false);
 
   const getData = useCallback(async () => {
-    if (localStorage.getItem("jsonData")) {
-      const datafromLocalstorage = await JSON.parse(
-        localStorage.getItem("jsonData") || ""
-      );
-      setApiData(datafromLocalstorage);
-    } else {
-      const jsonData = await fetch(`${process.env.REACT_APP_API}/products`);
-      const data = await jsonData.json();
-      localStorage.setItem("jsonData", JSON.stringify(data));
-      setApiData(data);
+    try {
+      if (localStorage.getItem("jsonData")) {
+        const datafromLocalstorage = await JSON.parse(
+          localStorage.getItem("jsonData") || ""
+        );
+        setApiData(datafromLocalstorage);
+      } else {
+        const jsonData = await fetch(`${process.env.REACT_APP_API}/products`);
+        const data = await jsonData.json();
+        localStorage.setItem("jsonData", JSON.stringify(data));
+        setApiData(data);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }, []);
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    setIsOpenmodal(true);
   }, []);
 
   useEffect(() => {
     getData();
   }, [getData]);
+
+  const handleCloseModal = () => {
+    document.body.style.overflow = "unset";
+    setIsOpenmodal(false);
+  };
 
   return (
     <div className={styles.homeWrapper}>
@@ -60,8 +79,33 @@ const Home = () => {
           />
         ))}
       </div>
-      <MyModal open={false}>
-        <h1>This is modal</h1>
+      <MyModal open={isOpenmodal}>
+        <div className={styles.welcomeModalContainer}>
+          <OutsideClickHandler onOutsideClick={() => handleCloseModal()}>
+            <div className={styles.modalWrapper}>
+              <div className={styles.imgWrapper}>
+                <img src={welcomeImage} alt="" />
+              </div>
+              <h2>How are you {localStorage.getItem("userName")} ?</h2>
+              <div className={styles.greetingWrapper}>
+                <div
+                  onClick={() => handleCloseModal()}
+                  className={styles.iamGood}
+                >
+                  <img src={greetingThumb} alt="" />
+                  <h4> I'm good!</h4>
+                </div>
+                <div
+                  onClick={() => handleCloseModal()}
+                  className={styles.regularDay}
+                >
+                  <img src={sadfaceImage} alt="" />
+                  <h4>Just regular day!</h4>
+                </div>
+              </div>
+            </div>
+          </OutsideClickHandler>
+        </div>
       </MyModal>
     </div>
   );
