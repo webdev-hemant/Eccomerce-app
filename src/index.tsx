28da87f53d60cf8d9713e77245cb02f254ecd055
webdev-hemant <webdev.hemant@gmail.com>
2022-10-03 10:24:31 +0530

    error boundary added

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalContext from "context/GlobalContextProvider";
-import MyErrorBoundary from "components/errorBoundary/MyErrorBoundary";
 import "react-loading-skeleton/dist/skeleton.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import "cssGlobal/index.css";
+import { ErrorBoundary } from "components/errorBoundary/ClassErrorBoundary";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <MyErrorBoundary FallbackComponent={<h1>something went wrong</h1>}>
+    <ErrorBoundary>
       <GlobalContext>
         <App />
       </GlobalContext>
-    </MyErrorBoundary>
+    </ErrorBoundary>
   </React.StrictMode>
 );
